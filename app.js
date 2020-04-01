f487288c8717526088daa05e852f75dd5a0de7e5
Edison <ealfarod215@ulacit.ed.cr>
2020-03-31 21:40:19 -0600

    Agregando rutas en app.js

--- app.js
+++ app.js
@@ -22,7 +22,14 @@ var listaLicoresRouter = require("./routes/ListaLicores");
 var registroLicoresRouter = require("./routes/RegistroLicores");
 var listaVinosRouter = require("./routes/ListaVinos");
 var registroVinosRouter = require("./routes/RegistroVinos");
-
+var ListaEspecialesRouter = require("./routes/ListaEspeciales");
+var registroEpecialesRouter = require("./routes/RegistroEpeciales");
+var ListaMesasRouter = require("./routes/ListaMesas");
+var registroMesasRouter = require("./routes/RegistroMesas");
+var ListaEmpleadosRouter = require("./routes/ListaEmpleados");
+var registroEmpleadosRouter = require("./routes/RegistroEmpleados");
+var ListaPuestosRouter= require("./routes/ListaPuestos");
+var registroPuestosRouter= require("./routes/RegistroPuestos");
 var app = express();
 
 // view engine setup
@@ -40,13 +47,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/ingresoAlSistema', ingresoAlSistema);
+app.use('/ingresoAlSistema',ingresoAlSistema);
 app.use('/ReporteDeAyudaDeSistema', reporteAyudaSistem);
 app.use('/ReporteAyudaDeSeguridad', reporteAyudaSeguridad);
-app.use('/ListaDeRestaurantes', listaDeRestaurantesRouter);
-app.use('/RestaurantesRegistro', restaurantesRegistroRouter);
-app.use('/Buffet', buffetRouter);
-app.use('/RegistroComidaBuffet', registroComidaBuffetRouter);
+app.use('/ListaDeRestaurantes',listaDeRestaurantesRouter);
+app.use('/RestaurantesRegistro',restaurantesRegistroRouter);
+app.use('/Buffet',buffetRouter);
+app.use('/RegistroComidaBuffet',registroComidaBuffetRouter);
 app.use('/ListaBebidasCalientes', listaBebidasCalientesRouter);
 app.use('/RegistroBebidasCalientes', registroBebidasCalientesRouter);
 app.use('/ListaBebidasHeladas', listaBebidasHeladasRouter);
@@ -57,14 +64,21 @@ app.use('/ListaLicores', listaLicoresRouter);
 app.use('/RegistroLicores', registroLicoresRouter);
 app.use('/ListaVinos', listaVinosRouter);
 app.use('/RegistroVinos', registroVinosRouter);
-
+app.use('/ListaEspeciales', ListaEspecialesRouter);
+app.use('/RegistroEpeciales', registroEpecialesRouter);
+app.use('/ListaMesas', ListaMesasRouter);
+app.use('/RegistroMesas', registroMesasRouter);
+app.use('/ListaEmpleados', ListaEmpleadosRouter);
+app.use('/RegistroEmpleados', registroEmpleadosRouter);
+app.use('/ListaPuestos', ListaPuestosRouter);
+app.use('/RegistroPuestos', registroPuestosRouter);
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
