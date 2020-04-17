7ff952da01ce33ffaa529b7b29f402fd6af4e522
crojasr757 <39276286+crojasr757@users.noreply.github.com>
2020-04-17 10:50:23 -0600

    USUARIOS  Y CONSECUTIVOS LISTOS PERFECTAMENTE
    Unicos cambios en la base de datos son de USUARIOS

--- routes/RegistroUsuarios.js
+++ routes/RegistroUsuarios.js
@@ -11,16 +11,13 @@ router.post('/insertarUsuarios', function (req, res, next) {
     var telefonoFijo = req.body.inputTelFijo;
     var telefonoCelular = req.body.inputTelCel;
     var loginUsuario = req.body.inputLogin;
-    var AdmSistema = req.body.inputAdmSistema;
-    var AdmSeguridad = req.body.inputAdmSeguridad;
-    var AdmRestaurante = req.body.inputAdmRestaurante;
-    var AdmCuentas = req.body.inputAdmCuentas;
+    var privilegios=req.body.inputprivilegios;
     var contrasena = req.body.inputClave;
     var confirmar = req.body.inputConfirmarClave;
 
-    if (AdmSistema == false || AdmSeguridad == false || AdmRestaurante == false || AdmCuentas == true || contrasena == confirmar) {
+    if (contrasena == confirmar) {
         db.query("EXEC sp_insertarUsuario @nombre = '"+nombre+"',@apellidoUno='"+apellidoUno+"',@apellidoDos='"+apellidoDos+"',@telefonoFijo='"+telefonoFijo+ 
-        "',@telefonoCelular='"+telefonoCelular+ "',@privilegios='"+1+ "',@loginUsuario='"+loginUsuario+ "',@contrasena='"+contrasena+"'", function (error, recordset) {
+        "',@telefonoCelular='"+telefonoCelular+ "',@privilegios='"+privilegios+ "',@loginUsuario='"+loginUsuario+ "',@contrasena='"+contrasena+"'", function (error, recordset) {
             if (error) {
                 alert("Verifique que las dos contrasenas coincidan y que se seleccionara un privilegio");
                 console.log("wronginsert");
@@ -28,44 +25,9 @@ router.post('/insertarUsuarios', function (req, res, next) {
             } else {
                 res.render('RegistroUsuarios');
             }
-        });
-    } else if (AdmSistema == true ||  AdmSeguridad == false || AdmRestaurante == false ||  AdmCuentas == false || contrasena == confirmar) {
-        db.query("EXEC sp_insertarUsuario @nombre = '"+nombre+"',@apellidoUno='"+apellidoUno+"',@apellidoDos='"+apellidoDos+"',@telefonoFijo='"+telefonoFijo+ 
-        "',@telefonoCelular='"+telefonoCelular+ "',@privilegios='"+2+ "',@loginUsuario='"+loginUsuario+ "',@contrasena='"+contrasena+"'", function (error, recordset) {
-            if (error) {
-                alert("Verifique que las dos contrasenas coincidan y que se seleccionara un privilegio");
-                console.log("wronginsert");
-                return;
-            } else {
-                res.render('RegistroUsuarios');
-            }
-        });
-    } else if (AdmSistema == false ||  AdmSeguridad == true || AdmRestaurante == false ||  AdmCuentas == false || contrasena == confirmar) {
-    db.query("EXEC sp_insertarUsuario @nombre = '"+nombre+"',@apellidoUno='"+apellidoUno+"',@apellidoDos='"+apellidoDos+"',@telefonoFijo='"+telefonoFijo+ 
-    "',@telefonoCelular='"+telefonoCelular+ "',@privilegios='"+3+ "',@loginUsuario='"+loginUsuario+ "',@contrasena='"+contrasena+"'", function (error, recordset) {
-        if (error) {
-            alert("Verifique que las dos contrasenas coincidan y que se seleccionara un privilegio");
-            console.log("wronginsert");
-            return;
-        } else {
-            res.render('RegistroUsuarios');
-        }
-    });
-    } else if (AdmSistema == false ||  AdmSeguridad == false || AdmRestaurante == true ||  AdmCuentas == false || contrasena == confirmar) {
-    db.query("EXEC sp_insertarUsuario @nombre = '"+nombre+"',@apellidoUno='"+apellidoUno+"',@apellidoDos='"+apellidoDos+"',@telefonoFijo='"+telefonoFijo+ 
-    "',@telefonoCelular='"+telefonoCelular+ "',@privilegios='"+4+ "',@loginUsuario='"+loginUsuario+ "',@contrasena='"+contrasena+"'", function (error, recordset) {
-        if (error) {
-            alert("Verifique que las dos contrasenas coincidan y que se seleccionara un privilegio");
-            console.log("wronginsert");
-            return;
-        } else {
-            res.render('RegistroUsuarios');
-        }
-    });
-    } else {
-    res.render('RegistroUsuarios');
-}
+    });}
 
-});
+
+    });
 
 module.exports = router;
